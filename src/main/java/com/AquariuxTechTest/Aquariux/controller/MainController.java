6de7f1eae59ac3215781fa2f87e6bef52f6145c9
soonhian <soonhian95@gmail.com>
2023-07-09 02:06:08 +0800

    Task 2 Commit for API to retrieve

--- src/main/java/com/AquariuxTechTest/Aquariux/controller/MainController.java
+++ src/main/java/com/AquariuxTechTest/Aquariux/controller/MainController.java
@@ -1,16 +1,28 @@
 package com.AquariuxTechTest.Aquariux.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.AquariuxTechTest.Aquariux.entity.CryptoCoinPair;
+import com.AquariuxTechTest.Aquariux.repository.UserRepository;
+import com.AquariuxTechTest.Aquariux.service.CoinService;
+
 @RestController
 public class MainController {
 	
+	@Autowired
+	CoinService coinService;
+	
 	@GetMapping("/getInfo")
 	public String getUserDetails() {
-		return "test";
+		System.out.println("TEST " + this.coinService.getAggregatedPrice());
+		return this.coinService.getAggregatedPrice().toString();
+		//return "test";
 		
 	}
 	
--- src/main/java/com/AquariuxTechTest/Aquariux/service/CoinService.java
+++ src/main/java/com/AquariuxTechTest/Aquariux/service/CoinService.java
@@ -1,17 +1,29 @@
 package com.AquariuxTechTest.Aquariux.service;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.AquariuxTechTest.Aquariux.entity.CryptoCoinPair;
+import com.AquariuxTechTest.Aquariux.repository.CyptoCoinRepository;
+
+@Service
 public class CoinService {
 
-	public static final String externalBinanceURL = "https://api.binance.com/api/v3/ticker/bookTicker";
-	public static final String huobi = "https://api.huobi.pro/market/tickers";
-
-    private  RestTemplate restTemplate;
+	@Autowired
+	CyptoCoinRepository cyptoCoinRepository;
+	
+	public List<CryptoCoinPair> getAggregatedPrice() {
+		System.out.println("TEST " + this.cyptoCoinRepository.findAll());
+		return this.cyptoCoinRepository.findAll();
+		
+	}
 
 	
 }
